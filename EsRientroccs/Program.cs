aa448c06ddd4f05fd18b9f1fa843d95e9d4d05d1
manuel <manue@LENOVOPC>
2023-08-28 16:14:17 +0200

    Aggiungere i file di progetto.

---
+++ EsRientroccs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libreriaCS;
+
+namespace EsRientroccs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibreriaCS l = new LibreriaCS();
+        }
+    }
+}
