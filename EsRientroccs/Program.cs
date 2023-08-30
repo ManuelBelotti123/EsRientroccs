4705d4dfedca07f1eb87b6b8134c7d95258d44eb
manuel <manue@LENOVOPC>
2023-08-30 14:39:51 +0200

    Configurazione progetto

--- EsRientroccs/Program.cs
+++ EsRientroccs/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace EsRientroccs
         static void Main(string[] args)
         {
             LibreriaCS l = new LibreriaCS();
+
         }
     }
 }
